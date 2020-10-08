fccf1b86dbd5d840e64f04132c6a901f10174181
nummyn0rih <nummyn0rih@gmail.com>
2020-10-09 01:06:39 +0400

    refact styled App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import styled, { css } from 'styled-components';
 import { prefix } from 'inline-style-prefixer';
-import { CSSTransition } from 'react-transition-group';
 
 import Main from './components/Main';
 import Header from './components/Header';
@@ -14,36 +13,35 @@ import {
 const StyledApp = styled.div`
   display: grid;
   grid-template-columns: 350px auto;
-  grid-template-rows: max-content auto;
+  grid-template-rows: repeat(2, min-content) auto min-content;
   grid-template-areas: ${({ sidebarVisible }) => {
     if (sidebarVisible)
       return css`
     'sidebar header'
+    'sidebar content'
     'sidebar content'
     'sidebar footer';
     `;
     return css`
     'header header'
     'content content'
+    'content content'
     'footer footer';
     `;
   }};
   gap: 0.5rem;
+  align-items: center;
   min-height: 100vh;
   padding: 0.5rem;
   background: #e5e5e5;
 
   @media (max-width: 769px) {
     grid-template-columns: 1fr;
-    grid-template-areas: ${({ sidebarVisible }) => {
-      if (sidebarVisible)
-        return css`
+    grid-template-areas:
       'header'
       'sidebar'
       'content'
       'footer';
-      `;
-    }};
   }
 `;
 
